dbc6e166a8fc1be4ea6f3e0097fe9d4b3d971473
Dhanalaxmi-Musku <135241955+Dhanalaxmi-Musku@users.noreply.github.com>
2024-12-03 14:53:01 +0530

    Added:calculatelength

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class LineComparison {
 	private final double x1;
@@ -10,8 +11,22 @@ public class LineComparison {
         this.x2 = x2;
         this.y2 = y2;
     }
+    public double calculateLength() {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter x1:");
+		int x1=scan.nextInt();
+		System.out.println("Enter y1:");
+		int y1=scan.nextInt();
+		System.out.println("Enter x2:");
+		int x2=scan.nextInt();
+		System.out.println("Enter y2:");
+		int y2=scan.nextInt();
+		LineComparison line1 = new LineComparison(x1, y1, x2, y2);
+		System.out.println("Line 1 Length: " + line1.calculateLength());
 	}
 
 }
