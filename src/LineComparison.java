3fd15315217fd516702648ef0821fc5b7fbbe463
Dhanalaxmi-Musku <135241955+Dhanalaxmi-Musku@users.noreply.github.com>
2024-12-03 14:40:39 +0530

    First commit

---
+++ src/LineComparison.java
@@ -0,0 +1,17 @@
+
+public class LineComparison {
+	private final double x1;
+    private final double y1;
+    private final double x2;
+    private final double y2;
+    public LineComparison(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
